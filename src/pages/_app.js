5a7f6047ad3773666d393ddb3fd8f0529fcbdb64
eder0782 <eder0782@gmail.com>
2023-02-07 17:31:00 -0400

    criada a nav bar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react'
+// `@chakra-ui/theme` is a part of the base install with `@chakra-ui/react`
+import chakraTheme from '@chakra-ui/theme'
+
+const { Button } = chakraTheme.components
+
+const theme = extendBaseTheme({
+  components: {
+    Button,
+  },
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+  <ChakraBaseProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraBaseProvider>
+
+  )
 }
