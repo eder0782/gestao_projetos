22ae449e2ea110c8d0da3506911af1ee89ba55ca
eder0782 <eder0782@gmail.com>
2023-02-10 08:37:15 -0400

    testando vercel

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -118,7 +118,7 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
     >
       <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
         <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
-          Logo
+          Controle de Projetos
         </Text>
         <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
       </Flex>
