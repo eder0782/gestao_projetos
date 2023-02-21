26695efcdabf840fa66dbe562826cda48a2845ff
eder0782 <eder0782@gmail.com>
2023-02-21 16:33:55 -0400

    Criado o formProjetos

---
+++ src/components/Forms/FormProjetos.js
@@ -0,0 +1,117 @@
+import { useRef, useEffect, useState } from "react";
+
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Box,
+  Flex,
+  Textarea,
+} from "@chakra-ui/react";
+import { Collapse } from "@chakra-ui/react";
+
+export default function FormProjetos(props) {
+  const [fileName, setFileName] = useState([]);
+  const inpRef = useRef(null);
+  const fileRef = useRef(null);
+  function Cancelar() {
+    props.setInicio("");
+    props.setProjeto("");
+    props.vlrProjeto("");
+
+    props.close();
+  }
+
+  //ESSE USEEFFECT MONITORA SE O COMPONENTE ESTÁ VISÍVEL PARA PODER DAR FOCO
+  useEffect(() => {
+    if (props.isOpen) {
+      //Esse setTimeout foi colocado, pois tem que aguardar o componente aparecer
+      //para poder dar foco nele
+      //   setFileName([]);
+      setTimeout(() => {
+        inpRef.current.focus();
+      }, 200);
+    }
+  }, [props.isOpen]);
+
+  return (
+    <Collapse in={props.isOpen} animateOpacity>
+      <Box
+        borderRadius={"md"}
+        border="1px"
+        padding="5"
+        borderColor="blackAlpha.900"
+        maxWidth="500"
+        marginTop={"5"}
+        shadow={"lg"}
+        px={{ base: 2, md: 7 }}
+        py={"5"}
+        // p="6"
+        rounded="md"
+      >
+        <FormControl width="300" isRequired>
+          <form>
+            <FormLabel>Projeto</FormLabel>
+            <Input
+              ref={inpRef}
+              value={props.projeto.toUpperCase()}
+              onChange={(e) =>
+                //converte o valor para maiusculo
+                props.setProjeto(e.currentTarget.value.toUpperCase())
+              }
+              type="text"
+              borderColor={"blackAlpha.600"}
+              placeholder="Fornecedor...."
+            />
+
+            <Flex justifyContent={"space-between"}>
+              <Box>
+                <FormLabel>Data Inicio</FormLabel>
+                <Input
+                  // defaultValue={Date.now()}
+                  value={props.inicio}
+                  onChange={(e) => props.setInicio(e.currentTarget.value)}
+                  type="date"
+                  borderColor={"blackAlpha.600"}
+                />
+              </Box>
+              <Box marginLeft={5}>
+                <FormLabel>Vlr Projeto</FormLabel>
+                <Input
+                  value={props.vlrProjeto}
+                  onChange={(e) => props.setVlrProjeto(e.currentTarget.value)}
+                  type="number"
+                  placeholder="1200,50"
+                  borderColor={"blackAlpha.600"}
+                />
+              </Box>
+            </Flex>
+            <FormLabel>Arquivo</FormLabel>
+            <Input
+              type="file"
+              value={fileName}
+              onChange={(e) => {
+                setFileName(e.currentTarget.value);
+              }}
+              ref={fileRef}
+              borderColor={"blackAlpha.600"}
+            />
+            <Flex justifyContent={"space-around"} marginTop={"5"}>
+              <Button
+                colorScheme="blue"
+                width={"8rem"}
+                onClick={() => props.submit(fileRef)}
+              >
+                Salvar
+              </Button>
+              <Button width={"8rem"} colorScheme="purple" onClick={Cancelar}>
+                Cancelar
+              </Button>
+            </Flex>
+          </form>
+        </FormControl>
+      </Box>
+    </Collapse>
+  );
+}
